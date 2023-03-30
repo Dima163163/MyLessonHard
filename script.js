0d47e9c2b50ca42765defe763492c67563f13b82
Dima163163 <khlopovdima@gmail.com>
2023-03-30 18:46:13 +0400

    отчистил ветку master

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let num = 266219;
 
-const arr = Array.from(String(num), Number);
-
-console.log(arr);
-
-const result = arr.reduce((acc, cur) => acc * cur);
-
-console.log(result);
-
-let deg = result ** 3;
-
-console.log(String(deg).slice(0, 2));
