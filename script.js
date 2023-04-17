c05c6ee1ba09366797c409f02d887a1a19229907
Dima163163 <khlopovdima@gmail.com>
2023-04-17 19:25:44 +0400

    исправил на коллекцию map

--- script.js
+++ script.js
@@ -2,46 +2,33 @@
 
 let lang = prompt("Какой язык выберете?");
 
-const langArr = {
-  ru: [
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-    "Воскресенье",
-  ],
-  en: [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ],
-};
+let map = new Map([
+  ["ru", "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье"],
+  ["en", "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"],
+]);
 
 if (lang == "ru") {
-  console.log(langArr["ru"]);
+  console.log(map.get("ru"));
 } else if (lang == "en") {
-  console.log(langArr["en"]);
+  console.log(map.get("en"));
 } else if (lang !== "ru" && lang !== "en") {
   console.log("Ошибка");
 }
 
 switch (lang) {
   case "ru":
-    console.log(langArr["ru"]);
+    console.log(map.get("ru"));
     break;
   case "en":
-    console.log(langArr["en"]);
+    console.log(map.get("en"));
     break;
   default:
     console.log("Ошибка");
     break;
 }
+
+console.log(map.get("ru"));
+console.log(map.get("en"));
 
 console.log(langArr["ru"]);
 console.log(langArr["en"]);
@@ -52,3 +39,54 @@ namePerson == "Артем"
   : namePerson == "Александр"
   ? console.log("преподаватель")
   : console.log("студент");
+
+// const langArr = {
+//   ru: [
+//     "Понедельник",
+//     "Вторник",
+//     "Среда",
+//     "Четверг",
+//     "Пятница",
+//     "Суббота",
+//     "Воскресенье",
+//   ],
+//   en: [
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//     "Sunday",
+//   ],
+// };
+
+// if (lang == "ru") {
+//   console.log(langArr["ru"]);
+// } else if (lang == "en") {
+//   console.log(langArr["en"]);
+// } else if (lang !== "ru" && lang !== "en") {
+//   console.log("Ошибка");
+// }
+
+// switch (lang) {
+//   case "ru":
+//     console.log(langArr["ru"]);
+//     break;
+//   case "en":
+//     console.log(langArr["en"]);
+//     break;
+//   default:
+//     console.log("Ошибка");
+//     break;
+// }
+
+// console.log(langArr["ru"]);
+// console.log(langArr["en"]);
+
+// let namePerson = prompt("Введите имя:");
+// namePerson == "Артем"
+//   ? console.log("Директор")
+//   : namePerson == "Александр"
+//   ? console.log("преподаватель")
+//   : console.log("студент");
