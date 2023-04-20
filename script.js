e709024118d9f5abc823d6ba6c20d00442c0e6fe
Dima163163 <khlopovdima@gmail.com>
2023-04-20 14:06:01 +0400

    lesson04 hard

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+"use strict";
 
+function newFunc(arg) {
+  if (typeof arg !== "string") {
+    alert("Это не строка");
+    return;
+  } else {
+    arg = arg.trim();
+    if (arg.length >= 29) {
+      return (arg = arg.slice(0, 29) + "...");
+    } else {
+      return arg;
+    }
+  }
+}
+
+console.log(newFunc("Это строка строка строка строка"));
