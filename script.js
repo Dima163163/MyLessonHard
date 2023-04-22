683b930cbe2b729ba3a77ba436b30b4ab2545bbb
Dima163163 <khlopovdima@gmail.com>
2023-04-22 23:18:07 +0400

    lesson05

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ for (let i = 2; i <= 100; i++) {
     console.log("Делители этого числа: 1 и " + i);
   }
 }
+
+let n = 100;
+
+step: for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue step;
+  }
+  console.log("Делители этого числа 1 и " + i);
+}
