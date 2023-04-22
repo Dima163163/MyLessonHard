48df138b9a6a8d20faae9cf15fc390e5859b28a7
Dima163163 <khlopovdima@gmail.com>
2023-04-22 17:36:58 +0400

    lesson05

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let arr = ["12", "424", "26", "56", "28", "484", "652"];
 
+arr.forEach((number) => {
+  if (number.startsWith("2") || number.startsWith("4")) {
+    console.log(number);
+  }
+});
+
+let n = 100;
+
+step: for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue step;
+  }
+  console.log("Делители этого числа 1 и " + i);
+}
