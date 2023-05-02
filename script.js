70953526141a34b311846d61f93867877ebd37f7
Dima163163 <khlopovdima@gmail.com>
2023-05-02 17:21:14 +0400

    lesson09hard

--- script.js
+++ script.js
@@ -1,25 +1,91 @@
 "use strict";
-const arr = ["12", "424", "26", "56", "28", "484", "652"];
+window.onload = function () {
+  window.setInterval(function () {
+    let myDate = new Date();
 
-arr.forEach((number) => {
-  if (number.startsWith("2") || number.startsWith("4")) {
-    console.log(number);
-  }
-});
+    let currentTime = myDate.toLocaleTimeString();
+    let hour = myDate.getHours();
+    let minute = myDate.getMinutes();
+    let second = myDate.getSeconds();
+    let message = "";
+    let days = [
+      "Понедельник",
+      "Вторник",
+      "Среда",
+      "Четверг",
+      "Пятница",
+      "Суббота",
+      "Воскресенье",
+    ];
 
-for (let i = 2; i <= 100; i++) {
-  if (i == 2 || i == 3 || i == 5 || i == 7) {
-    console.log("Делители этого числа: 1 и " + i);
-  }
-  if (i % 2 && i % 3 && i % 5 && i % 7) {
-    console.log("Делители этого числа: 1 и " + i);
-  }
-}
-const n = 100;
+    let months = [
+      "Января",
+      "Февраля",
+      "Марта",
+      "Апреле",
+      "Мая",
+      "Июня",
+      "Июля",
+      "Августа",
+      "Сентября",
+      "Октября",
+      "Ноября",
+      "Декабря",
+    ];
 
-step: for (let i = 2; i <= n; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue step;
-  }
-  console.log("Делители этого числа: 1 и " + i);
-}
+    function timeName(number, one, two, five) {
+      let n = Math.abs(number);
+      n %= 100;
+      if (n >= 5 && n <= 20) {
+        return five;
+      }
+      n %= 10;
+      if (n === 1) {
+        return one;
+      }
+      if (n >= 2 && n <= 4) {
+        return two;
+      }
+      return five;
+    }
+    timeName();
+
+    function oneNumber(time) {
+      if (time < 10) {
+        time = "0" + time;
+      } else {
+        time = time;
+      }
+      return time;
+    }
+
+    oneNumber();
+
+    message +=
+      "Сегодня: " +
+      days[myDate.getDate() - 1] +
+      ", " +
+      myDate.getDate() +
+      " " +
+      months[myDate.getMonth()] +
+      " " +
+      myDate.getFullYear() +
+      ", " +
+      oneNumber(hour) +
+      " " +
+      timeName(hour, "час", "часа", "часов") +
+      " " +
+      ":" +
+      oneNumber(minute) +
+      " " +
+      timeName(minute, "минута", "минуты", "минут") +
+      " " +
+      ":" +
+      oneNumber(second) +
+      " " +
+      timeName(second, "секунда", "секунды", "секунд") +
+      "<br />" +
+      currentTime;
+    document.getElementById("clock").innerHTML = message;
+  });
+};
